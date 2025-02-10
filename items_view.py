874d8629a9055a82ad5413bec268717d3af36df3
intrafellow <145547700+intrafellow@users.noreply.github.com>
2025-02-10 15:19:32 +0300

    Added SQLite3 + created CRUD for products using async

--- items_view.py
+++ items_view.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter,Path
+from fastapi import APIRouter, Path
 
 router = APIRouter(prefix="/items", tags=["Items"])
 
@@ -9,19 +9,18 @@ def items_list():
     return [
         "Item1",
         "Item2",
-        "Item3"
+        "Item3",
     ]
 
 
 @router.get("/latest/")
 def get_latest_item():
-    return {"id" : "0", "name": "latest"}
+    return {
+        "id": "0",
+        "name": "latest",
+    }
 
 
 @router.get("/{item_id}/")
 def get_item_by_id(item_id: Annotated[int, Path(ge=1)]):
-    return {
-        'Item': {
-            "id" : item_id
-        }
-    },
+    return ({"Item": {"id": item_id}},)
