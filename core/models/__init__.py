ee22222115deb5a4a554982250276a99b0fce17e
intrafellow <145547700+intrafellow@users.noreply.github.com>
2025-02-27 14:05:36 +0300

    Created user model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,12 @@
-__all__ = ("Base", "Product", "DatabaseHelper", "db_helper")
+__all__ = (
+    "Base",
+    "Product",
+    "DatabaseHelper",
+    "db_helper",
+    "User",
+)
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .product import Product
+from .user import User
