81712c7630d40f8d0473db2f4f56b425982a8566
intrafellow <145547700+intrafellow@users.noreply.github.com>
2025-03-03 15:16:52 +0300

    Created relationship between user and post

--- core/models/post.py
+++ core/models/post.py
@@ -1,7 +1,11 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Text, ForeignKey
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Post(Base):
@@ -12,7 +16,7 @@ class Post(Base):
         default="",
         server_default="",
     )
-
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id"),
     )
+    user: Mapped["User"] = relationship(back_populates="posts")
